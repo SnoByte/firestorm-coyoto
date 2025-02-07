21f08f5f255e6e85964688964a225841168422eb
minerjr <minerjrminerjr@hotmail.com>
2025-02-07 08:44:01 -0400

    Added missing new atomic values for Linux and OSX

--- indra/llcommon/llatomic.h
+++ indra/llcommon/llatomic.h
@@ -67,6 +67,11 @@ typedef LLAtomicBase<S64, std::atomic_int64_t> LLAtomicS64;
 #else
 typedef LLAtomicBase<U32, std::atomic_uint> LLAtomicU32;
 typedef LLAtomicBase<S32, std::atomic_int> LLAtomicS32;
+// <FS:minerjr>
+// Added 64bit atomic values for linux, osx
+typedef LLAtomicBase<U64, std::atomic_ullong> LLAtomicU64;
+typedef LLAtomicBase<S64, std::atomic_llong> LLAtomicS64;
+// <FS:minerjr>
 #endif
 
 typedef LLAtomicBase<bool, std::atomic_bool> LLAtomicBool;
